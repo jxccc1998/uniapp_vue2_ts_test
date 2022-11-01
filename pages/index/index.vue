c41af47d10ba938af4c0577507b43905f67a8af9
jxccc1998 <970047273@qq.com>
2022-11-01 17:10:09 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,52 @@
+<template>
+	<view class="content">
+		<view v-for="item in list">
+			{{ item.a }}
+		</view>
+	</view>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue'
+	export default Vue.extend({
+		data() {
+			return {
+				list: []
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	})
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
